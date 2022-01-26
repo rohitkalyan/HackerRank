9f34d29d93e32e7a5ca03a922982e240827a125d
rohitkalyan <38258084+rohitkalyan@users.noreply.github.com>
2022-01-26 16:53:37 +0530

    Create Compare the Triplets.scala

---
+++ Scala/ProblemSolving/Compare the Triplets.scala
@@ -0,0 +1,42 @@
+import java.io._
+import java.math._
+import java.security._
+import java.text._
+import java.util._
+import java.util.concurrent._
+import java.util.function._
+import java.util.regex._
+import java.util.stream._
+import scala.collection.immutable._
+import scala.collection.mutable._
+import scala.collection.concurrent._
+import scala.concurrent._
+import scala.io._
+import scala.math._
+import scala.sys._
+import scala.util.matching._
+import scala.reflect._
+
+object Result {
+    def compareTriplets(a: Array[Int], b: Array[Int]): Array[Int] = {
+    // Write your code here
+
+    }
+
+}
+
+object Solution {
+    def main(args: Array[String]) {
+        val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
+
+        val a = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
+
+        val b = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
+
+        val result = Result.compareTriplets(a, b)
+
+        printWriter.println(result.mkString(" "))
+
+        printWriter.close()
+    }
+}
